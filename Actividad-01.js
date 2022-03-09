8655f3b998b6ebed15005fc0d36b15263f232ab2
Gabriela Cervantes <gabydc06@gmail.com>
2022-03-08 20:34:54 -0600

    Acividad01

---
+++ Actividad-01.js
@@ -0,0 +1,34 @@
+// multiplicación 
+
+var numero = Number(prompt("Ingresa un número"))
+var operacion = numero * 2
+console.log(operacion)
+VM907:3 54
+undefined
+
+// resta
+
+var numeroUno = Number(prompt("Ingresa un número"))
+var numeroDos = Number(prompt("Ingresa otro número"))
+var operacion = numeroUno - numeroDos
+console.log(operacion)
+VM1500:4 10
+undefined
+
+// área circulo
+
+var radio = 5.3
+var pi = 3.1416
+var operacion = (pi * radio)**2
+console.log(operacion)
+VM1722:4 277.23848423039993
+undefined
+
+// área rombo
+
+var diagonalMayor = 8
+var diagonalMenor = 6
+var operacion = (diagonalMayor * diagonalMenor) / 2
+console.log(operacion)
+VM2071:4 24
+undefined
